3983d86f3f3b2cff4f766abf990a16eb8d11d4cf
Adrian Oceguera <adrianoceguera@maderausd.org>
2021-07-02 14:34:54 -0700

    Progress

--- js/disc-golf-weekly.js
+++ js/disc-golf-weekly.js
@@ -33,13 +33,178 @@ const DG = {
                 { text : '6', value : 6 },
                 { text : 'All on One Card', value : 'all' },
                 // { label : '1', value : 1 },
-            ]
+            ],
+
+            persons : [
+                {
+                    name : '',
+                    isTD : true,
+                    highLowOptions : [
+                        {
+                            text : 'No Division',
+                            value : 'none'
+                        },
+                        {
+                            text : 'High',
+                            value : 'high'
+                        },
+                        {
+                            text : 'Middle',
+                            value : 'middle'
+                        },
+                        {
+                            text : 'Low',
+                            value : 'low'
+                        },
+                    ],
+                    highLowSelect : 'none'
+                }
+            ],
+
+            assignmentError : ''
         }
     },
 
-    methods() {
+    computed : {
+        disableDivision : function() {
+            return this.highLowOrRandom === 'random' || this.peoplePerTeam === 1;
+        }
+    },
+
+    methods : {
+
+        minMaxChange : function() {
+            if( this.minimumPerCard > this.maximumPerCard ) {
+                alert( 'Minimum per card cannot be higher than the maximum per card. Fix please!' );
+            }
+
+            if( ( this.minimumPerCard === 'all' && this.maximumPerCard !== 'all' ) || ( this.minimumPerCard !== 'all' && this.maximumPerCard === 'all' ) ) {
+                alert( 'You changed one max min per card setting to "All on One Card". Make sure to change the other!' );
+            }
+        },
+
+        addPlayer : function() {
+            const allFilled = this.persons.reduce( ( acc, cur ) => {
+                if( acc === false ) return false;
+
+                acc = cur.name !== '';
+
+                return acc;
+            }, true );
+
+            if( !allFilled ) {
+                alert( 'Must fill in all previous names before adding another' );
+                return;
+            }
+
+            this.persons.push( {
+                name : '',
+                isTD : false,
+                highLowOptions : [
+                    {
+                        text : 'High',
+                        value : 'high'
+                    },
+                    {
+                        text : 'Middle',
+                        value : 'middle'
+                    },
+                    {
+                        text : 'Low',
+                        value : 'low'
+                    },
+                ],
+                highLowSelect : 'low'
+            } );
+
+            Vue.nextTick()
+                .then( () => {
+                    const players = document.getElementsByClassName( 'playerNames' );
+                    players[players.length - 1].focus();
+                } );
+
+        },
+
+        removePlayer : function( playerIndex ) {
+
+            if( this.persons.length === 1 ) {
+                alert( 'Cannot bring total players to zero!' );
+                return;
+            }
+
+            this.persons.splice( playerIndex, 1 );
+
+        },
+
+        assignCards : function( playersOrTeams ) {
+
+        },
+
+        getHighLowTeams : function( players ) {
+
+            const divisionObject = players.reduce( ( acc, cur ) => {
+
+                acc[cur.highLowSelect].push( cur );
+                return acc;
+
+            }, {
+                low : [],
+                middle : [],
+                high : []
+            } );
 
 
+
+        },
+
+        getRandomTeams : function( players ) {
+
+        },
+
+        assignTeams : function( players ) { // players is an array of objects
+
+            let teams;
+            if( this.highLowOrRandom === 'highLow' ) {
+                teams = this.getHighLowTeams( players );
+            }
+
+            return teams;
+
+        },
+
+        assignPlayers : function() {
+
+            const usefulPlayers = this.persons.filter( el => el.name !== '' );
+
+            if( usefulPlayers.length === 0 ) {
+                alert( 'Add at least one player!' );
+                return;
+            }
+
+            if( this.peoplePerTeam === 1 ) {
+                this.assignCards( usefulPlayers );
+            }
+
+            if( this.peoplePerTeam === 2 && this.highLowOrRandom === 'highLow' ) {
+                const allDivisionsAssigned = usefulPlayers.reduce( ( acc, cur ) => {
+
+                    if( acc === false ) return acc;
+
+                    acc = cur.name !== '';
+
+                    return acc;
+
+                }, true );
+
+                if( !allDivisionsAssigned ) {
+                    alert( 'Not every player has been assigned a division!' );
+                    return;
+                }
+            }
+
+            this.assignTeams( usefulPlayers );
+
+        }
 
     }
 }
